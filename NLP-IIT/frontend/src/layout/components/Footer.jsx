aefe1269b26d4ccd0a6ea8d91f688d08c44a27a9
abj-paul <bsse1201@iit.du.ac.bd>
2024-09-07 03:13:23 +0600

    Migrating from iitdu-nlp.github.io to barta-research.github.io

---
+++ NLP-IIT/frontend/src/layout/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer className="bg-[#050615] text-white p-10">
+        <div className="text-center font-extralight">
+          <h3 className="text-3xl mb-5">Contact</h3>
+          <h4>Email: barta.research@gmail.com</h4>
+          <h5>Contact: +8801968399154</h5>
+          <h5>IIT, University of Dhaka, Bangladesh</h5>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
